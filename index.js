09288056b3b1288834a8a4bfe0f9a30b9ac3e6a7
LaurinWindhaus <101179832+LaurinWindhaus@users.noreply.github.com>
2025-01-22 15:44:43 +0100

    Update index.js
    test

--- index.js
+++ index.js
@@ -3,11 +3,7 @@ const app = express();
 const port = 3000;
 
 app.get('/', (req, res) => {
-  res.send('Hello, World! - CI/CD works fine!');
-});
-
-app.get('/api', (req, res) => {
-  res.json({ message: 'Hello from the API!' });
+  res.send('Hello, World! - CI/CD works fine!!!');
 });
 
 app.listen(port, () => {
